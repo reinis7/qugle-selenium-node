d8e4ad3077828838bf33b59d391f3f40930f68b9
olegmicode <olegmijovic.io@gmail.com>
2025-10-17 19:29:20 +0200

    fix: script and package.json

--- scripts/start-xvfb.sh
+++ scripts/start-xvfb.sh
@@ -12,9 +12,12 @@ fi
 # Default values if not set
 DISPLAY="${DISPLAY:-:1}"
 SCREEN="${SCREEN:-1440x900x24}"
+echo $DISPLAY;
+echo $SCREEN;
 
 # Start Xvfb if not already up
 if ! xdpyinfo -display "$DISPLAY" >/dev/null 2>&1; then
+  echo "start xvfb"
   nohup Xvfb "$DISPLAY" -screen 0 "$SCREEN" -nolisten tcp -noreset >/tmp/xvfb.log 2>&1 &
   # small wait for server to come up
   for i in {1..20}; do
